0543f5d458c36e1a7956ec128d346b578baa42d8
gianthk <gianthk.iori@gmail.com>
2022-08-05 11:38:23 +0300

    ReadTheDocs build config file

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# -- Options for {MyST}NB ----------------------------------------------------
+nb_kernel_rgx_aliases = {
+
+}
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
