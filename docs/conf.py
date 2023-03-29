d90b9398a62983810862faf7160141b16ab77be7
gianthk <gianthk.iori@gmail.com>
2023-03-29 20:38:49 +0300

    fixed bug causing rectangular base models to fail

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2023, Open and Reproducible MSK Imaging Research - ORMIR'
 author = 'Gianluca Iori'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.2'
+release = '1.3.5'
 
 # -- General configuration ---------------------------------------------------
 
--- src/ciclope/__init__.py
+++ src/ciclope/__init__.py
@@ -1,5 +1,5 @@
 """Computed Tomography to Finite Elements"""
-__version__ = "1.3.4"
+__version__ = "1.3.5"
 
 from .core import voxelFE, tetraFE
 # from .utils import preprocess, postprocess
