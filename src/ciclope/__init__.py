fc0ec0a0fcabac7f7f185f78ab61a7f3a7d07308
giiori <giiori@ethz.ch>
2025-11-04 14:16:51 +0100

    do not install tetraFE modules

--- src/ciclope/__init__.py
+++ src/ciclope/__init__.py
@@ -1,5 +1,5 @@
 """Computed Tomography to Finite Elements"""
 __version__ = "2.0.0"
 
-from .core import voxelFE, tetraFE
+from .core import voxelFE #, tetraFE
 # from .utils import preprocess, postprocess
